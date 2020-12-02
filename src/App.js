091cc2feca269d78f3130d735bd29e580fe0d493
Eric D. Torres <etorresnotary@gmail.com>
2020-12-02 14:04:24 -0600

    search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Searchbox from "./components/Searchbox/Searchbox";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Searchbox />
     </div>
   );
 }
