1324df7252fc34af37c3e1ad25c5b39b31e06a9c
Eric D. Torres <etorresnotary@gmail.com>
2020-12-02 14:24:51 -0600

    card list

--- src/components/card/Cardlist.js
+++ src/components/card/Cardlist.js
@@ -1,1 +1,15 @@
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import "./Cardlist"
 
+const Cardlist = (props) => {
+    return (
+        <div className="CardList">
+        {props.user.map((user) => (
+            <Card key={user.id} user={user} />
+        ))}
+        </div>
+    )
+}
+
+export default Cardlist
